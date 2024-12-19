4e26ff3e1bcce8991e3ed3ab1038f1376321cfee
Sabbir Hossain <sabbir01619@gmail.com>
2024-12-20 02:55:19 +0600

    Added compare_two_numbers.asm

---
+++ compare_two_numbers.asm
@@ -0,0 +1,28 @@
+; Author: Sabbir Hossain
+
+; Compare two numbers
+; 1st < 2nd -> DX = 1h
+; 1st > 2nd -> DX = 2h
+; 1st = 2nd -> DX = 3h
+
+ORG 100h
+
+MOV AX, 10h
+MOV BX, 30h
+MOV DX, 03h
+CMP AX, BX
+JZ EQUAL
+JC SMALLER
+JNC GREATER
+
+EQUAL:
+  MOV DX, 03h
+  RET
+
+SMALLER:
+  MOV DX, 01h
+  RET
+
+GREATER:
+  MOV DX, 02h
+  RET
